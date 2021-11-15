8ed19cf19dd532e79c04dd93d9d1a075c4f17ff5
smiley717 <smiley.dev717@gmail.com>
2021-11-15 04:56:50 -0500

    Create HelpLabel.tsx

---
+++ src/components/HelpLabel.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function ConnectButton() {
+  return (
+    <Box display="flex" alignItems="center" py="0" marginLeft="20px">
+      <Text color="white" fontSize="md">
+        To see more lands, move your mouse move up or down
+      </Text>
+    </Box>
+  );
+}
