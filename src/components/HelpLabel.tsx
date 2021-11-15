8cfea092b18a28ffd6507adf5e3155a2dd932250
smiley717 <smiley.dev717@gmail.com>
2021-11-15 07:16:53 -0500

    Update HelpLabel.tsx

--- src/components/HelpLabel.tsx
+++ src/components/HelpLabel.tsx
@@ -4,7 +4,7 @@ export default function ConnectButton() {
   return (
     <Box display="flex" alignItems="center" py="0" marginLeft="20px">
       <Text color="white" fontSize="md">
-        To see more lands, move your mouse move up or down
+        To see more lands, move your mouse up or down
       </Text>
     </Box>
   );
