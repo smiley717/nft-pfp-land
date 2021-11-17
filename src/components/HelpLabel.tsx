ae2be22bd95b9a82c0735379f5fce955f41c69b4
smiley717 <smiley.dev717@gmail.com>
2021-11-17 04:54:04 -0500

    mobile responsive

--- src/components/HelpLabel.tsx
+++ src/components/HelpLabel.tsx
@@ -1,11 +1,16 @@
 import { Box, Text } from "@chakra-ui/react";
 
 export default function ConnectButton() {
+  const isMobile = window.screen.width <= window.screen.height ? true : false;
   return (
     <Box display="flex" alignItems="center" py="0" marginLeft="20px">
-      <Text color="white" fontSize="md">
-        To see more lands, move your mouse up or down
-      </Text>
+      {isMobile ? (
+        <></>
+      ) : (
+        <Text color="white" fontSize="md">
+          To see more lands, move your mouse up or down
+        </Text>
+      )}
     </Box>
   );
 }
